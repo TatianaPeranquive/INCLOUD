4479eaf0a12476e3fe80271e770162e655a7805a
Anggy_peranquive <tatianaperanquive@gmail.com>
2024-05-22 21:18:15 -0500

    Creación de controladores y modelos pro_proceso y doc_documento (AP)

---
+++ app/Http/Controllers/pro_proceso_controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pro_proceso;
+use Illuminate\Http\Request;
+
+class pro_proceso_controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, pro_proceso $pro_proceso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(pro_proceso $pro_proceso)
+    {
+        //
+    }
+}
